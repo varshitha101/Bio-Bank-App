090d17626f64e07573800a10f6e91dd9a57145ee
varshitha101 <95077762+varshitha101@users.noreply.github.com>
2024-12-19 11:48:00 +0530

    Db Changed to Deplyment

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 // Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDIFI_4lVb7FJmKgzWMbq6ZfKcBwpj-K4E",
-  authDomain: "biobank-development.firebaseapp.com",
-  databaseURL: "https://biobank-development-default-rtdb.firebaseio.com",
-  projectId: "biobank-development",
-  storageBucket: "biobank-development.firebasestorage.app",
-  messagingSenderId: "31278898937",
-  appId: "1:31278898937:web:01f96df7a640d9c1410c28",
-  measurementId: "G-B98TGR5Q8Q"
-};
+//Devolpment
 // const firebaseConfig = {
-//   apiKey: "AIzaSyCbpb_1jb6mDvF_7kuN8J0lwIoW7-mKd8g",
-//   authDomain: "bio-bank-deployment.firebaseapp.com",
-//   databaseURL: "https://bio-bank-deployment-default-rtdb.firebaseio.com",
-//   projectId: "bio-bank-deployment",
-//   storageBucket: "bio-bank-deployment.firebasestorage.app",
-//   messagingSenderId: "674946404975",
-//   appId: "1:674946404975:web:777e4171f5b473e6b3f39a",
-//   measurementId: "G-MQP97GW8F9"
+//   apiKey: "AIzaSyDIFI_4lVb7FJmKgzWMbq6ZfKcBwpj-K4E",
+//   authDomain: "biobank-development.firebaseapp.com",
+//   databaseURL: "https://biobank-development-default-rtdb.firebaseio.com",
+//   projectId: "biobank-development",
+//   storageBucket: "biobank-development.firebasestorage.app",
+//   messagingSenderId: "31278898937",
+//   appId: "1:31278898937:web:01f96df7a640d9c1410c28",
+//   measurementId: "G-B98TGR5Q8Q"
 // };
+//Deployment
+const firebaseConfig = {
+  apiKey: "AIzaSyCbpb_1jb6mDvF_7kuN8J0lwIoW7-mKd8g",
+  authDomain: "bio-bank-deployment.firebaseapp.com",
+  databaseURL: "https://bio-bank-deployment-default-rtdb.firebaseio.com",
+  projectId: "bio-bank-deployment",
+  storageBucket: "bio-bank-deployment.firebasestorage.app",
+  messagingSenderId: "674946404975",
+  appId: "1:674946404975:web:777e4171f5b473e6b3f39a",
+  measurementId: "G-MQP97GW8F9"
+};
 
 
 
